c85e7865f0e1c9f127f31169529be340f33a36ff
alexdizz <87606020+alexdizz@users.noreply.github.com>
2022-02-15 19:37:14 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(function(){
+    $('.header-slider').slick({
+        dots: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="images/arrowUp.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="images/arrowDown.svg" alt=""></button>',
+        fade: true
+    });
+});
